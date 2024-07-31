446964a23647ab16d3e4ca75ed7a4ea4e46c5226
Lucas-allard <100565368+kaluxxx@users.noreply.github.com>
2024-07-31 23:57:35 +0200

    add image update + follow/unfollow profile

--- src/app/profile/[wallet]/page.tsx
+++ src/app/profile/[wallet]/page.tsx
@@ -333,7 +333,7 @@ export default function ProfilePage() {
                                         {followers.map((follower) => (
                                             <div key={follower.id} className="flex items-center gap-2">
                                                 <div className="relative">
-                                                    <Image src={follower.image} alt="Profile"
+                                                    <Image src={`/uploads/${follower.image}`} alt="Profile"
                                                            className="w-10 h-10 rounded-full"
                                                            width={40} height={40}/>
                                                 </div>
