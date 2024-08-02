2cd9dc5d9112aeab3fd7ad5c4c25958a4dd174db
Lucas-allard <100565368+kaluxxx@users.noreply.github.com>
2024-08-03 00:04:40 +0200

    add admin layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { WalletConnectionProvider } from '@/provider/wallet-connection-provider';
+import {WalletConnectionProvider} from '@/provider/wallet-connection-provider';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import "@/app/globals.css";
 import {getServerSession} from "next-auth";
+import {Toaster} from "react-hot-toast";
+
 
 export default async function RootLayout({children}: { children: React.ReactNode }) {
     const session = await getServerSession();
@@ -11,13 +13,19 @@ export default async function RootLayout({children}: { children: React.ReactNode
     return (
         <html lang="en">
         <body className="min-h-screen bg-image font-sans antialiased flex flex-col justify-between">
-            <WalletConnectionProvider session={session}>
+        <WalletConnectionProvider session={session}>
                 <Header/>
                 <main className="flex-1 w-full flex flex-col items-center">
                     {children}
                 </main>
                 <Footer/>
-            </WalletConnectionProvider>
+                <Toaster
+                    position="top-right"
+                    toastOptions={{
+                        className: 'border border-primary/20',
+                    }}
+                />
+        </WalletConnectionProvider>
         </body>
         </html>
     );
